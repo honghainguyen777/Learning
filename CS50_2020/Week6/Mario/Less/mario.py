7d4f13b8bdd92916e0f217b2e8b502aacbaaa198
honghainguyen777 <n.h.hai2015@gmail.com>
2020-02-01 11:45:03 +0100

    submit pset6 Hello and Mario

---
+++ CS50_2020/Week6/Mario/Less/mario.py
@@ -0,0 +1,15 @@
+# import get_int from cs50 library
+from cs50 import get_int
+
+# Initialize condition
+require = False
+while not require:
+    # prompt the user to the size of the half-pyramid
+    n = get_int("Height: ")
+    if n >= 1 and n <= 8:
+        require = True
+
+# go through each line
+for i in range(n):
+    # print hashes in each line
+    print(" " * (n - i - 1) + "#" * (i + 1))
