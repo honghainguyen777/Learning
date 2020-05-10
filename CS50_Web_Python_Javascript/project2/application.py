c7a3d2a4885008d525595cdf3be8c389214f9aa3
honghainguyen777 <n.h.hai2015@gmail.com>
2020-05-11 00:36:54 +0200

    progress in project2-CS50Web

--- CS50_Web_Python_Javascript/project2/application.py
+++ CS50_Web_Python_Javascript/project2/application.py
@@ -2,13 +2,14 @@ import os
 
 from flask import Flask, request, render_template, redirect, session, flash
 from flask_session import Session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from helpers import login_required
 from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+app.run(debug=True)
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -19,22 +20,25 @@ Session(app)
 channel_list = []
 
 # user list
-user_list = []
+user_list = ['thao', 'someone']
 
 # all messages from users
 user_messages = dict()
 
 
 @app.route("/")
-@login_required
 def index():
-    username = session["username"]
-    try:
-        session["selected_channel"]
-    except KeyError:
-        return render_template("index.html", username=username, channel_list=channel_list)
-    channel_name = session["selected_channel"]
-    return redirect("/channels/" + channel_name)
+    print(channel_list)
+    print(user_list)
+    print(user_messages)
+    if ("username" in session) and (session["username"] in user_list):
+        if "selected_channel" in session:
+            print(session["selected_channel"])
+            return redirect("/channels/" + session["selected_channel"])
+        return redirect('/create')
+    # make sure the username does not in session due to some bugs from other routes
+    session.clear()
+    return redirect('/register')
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -43,7 +47,7 @@ def register():
         if username in user_list:
             flash("Username already exists.")
             return render_template("register.html")
-        channel_list.append(username)
+        user_list.append(username)
         session["username"] = username
         flash("Register, please enjoy chatting!")
         return redirect("/")
@@ -51,9 +55,11 @@ def register():
         return render_template("register.html")
 
 @app.route("/logout")
+@login_required
 def logout():
+    print(user_list)
     username = session["username"]
-    user_list.remove(username)
+    user_list.remove(str(username))
     session.clear()
     return redirect("/")
 
@@ -65,26 +71,46 @@ def create():
         channel_name = request.form.get("channel_name")
         if channel_name in channel_list:
             flash("Channel name already exists.")
-            return render_template("create_channel.html")
+            return render_template("index.html")
         channel_list.append(channel_name)
         user_messages[channel_name] = []
+        session["selected_channel"] = channel_name
         return redirect("/channels/" + channel_name)
     else:
         return render_template("index.html")
 
-@app.route("/channels/<channel>", methods=["GET"])
+@app.route("/channels/<channel>", methods=["GET", "POST"])
 @login_required
 def channels(channel):
-    username = session["username"]
+    # Make sure user dont modify the path
     session["selected_channel"] = channel
-    print(user_messages)
-    messages = user_messages[channel]
-    return render_template("channel.html", channel=channel, username=username, messages=messages)
+    if (channel in channel_list):
+        username = session["username"]
+        messages = user_messages[channel]
+        return render_template("channel.html", channel=channel, username=username, messages=messages)
+    else:
+        flash("Channel does not exist")
+        return render_template("index.html")
+
+# join_room method: https://flask-socketio.readthedocs.io/en/latest/
+@socketio.on('joined')
+@login_required
+def on_join():
+    current_channel = session["selected_channel"]
+    username = session["username"]
+    emit('state', {"announce": username + " has joined"}, broadcast=True)
+
+@socketio.on('left')
+@login_required
+def on_leave():
+    username = session["username"]
+    emit('state', {"announce": username + " has left"}, broadcast=True)
 
 
 # dp socket.io for sending and storing messages
 @socketio.on("submit message")
 def message(data):
+    print(data)
     username = session["username"]
     sent_message = data["message"]
     now = datetime.now()
@@ -92,6 +118,7 @@ def message(data):
     current_channel = session["selected_channel"]
     message_data = [username, sent_message, recorded_time]
     user_messages[current_channel].append(message_data)
+    print(user_messages)
     if len(user_messages[current_channel]) > 100:
         user_messages[current_channel].popleft()
-    emit("broadcast", {"username": username, "message": sent_message, "channel": current_channel, "recorded_time": recorded_time}, broadcast=True)
+    emit("announce message", {"current_channel": current_channel, "message_data" :message_data}, broadcast=True)
--- CS50_Web_Python_Javascript/project2/static/index.js
+++ CS50_Web_Python_Javascript/project2/static/index.js
@@ -1,15 +1,101 @@
+function scrollToBottom() {
+    const element = document.getElementById("scroll_default");
+    const elementHeight = element.scrollHeight;
+    element.scrollTop = elementHeight;
+}
+
+
+// when the new message is added, call scrollToBottom. if the elementHeight === to scrollHeight
+// do nothing
+
 document.addEventListener('DOMContentLoaded', () => {
-  // Cnnect to websocket
+  // connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+  let localData = new Object();
 
-  // When connected, configure buttons
   socket.on('connect', () => {
-    document.querySelector('#smg button').onclick = () => {
-      const selection = document.getElementById("message").value;
-      socket.emit('submit message', {'selection': selection});
 
-      // delete the input in the typing field
-      document.getElementById("message").value = '';
-    };
+    var channel_name = document.getElementById("channel_name").textContent;
+    var username = document.getElementById("username").textContent;
+
+
+    if (localStorage.length === 0) {
+      localData[channel_name] = new Object();
+      localStorage.setItem(channel_name, JSON.stringify(localData[channel_name]));
+    } else {
+      console.log(localStorage.getItem(channel_name));
+      localData[channel_name] = JSON.parse(localStorage.getItem(channel_name));
+      console.log(localData[channel_name]);
+    }
+
+    // Grab all previous message
+
+    // When exit the room, delete all data in the room in the localStorage. hint in the state
+    // if state === 'left', then delete
+
+    // When signout, emit an announce and delete all localStorage: announce from Flask, and delete from local
+
+    // dont save anything in the flask, try to take the data from local and place it in flask
+
+    socket.emit('joined');
+
+    // The submit button should emit a 'submit message' event
+    document.querySelector('#send_message').addEventListener('click', () => {
+      let message = document.querySelector('.type_msg').value;
+      socket.emit('submit message', {'message': message});
+      document.querySelector('.type_msg').value = '';
+    });
+
+    document.querySelector('.type_msg').addEventListener('keydown', event => {
+      let send_message = document.querySelector('#send_message');
+      if (event.keyCode == 13) {
+        send_message.click();
+      }
+    });
+  });
+
+  socket.on('announce message', data => {
+    let message_data = data.message_data;
+    let current_channel = data.current_channel;
+    let existingLocalData = localData[current_channel];
+    console.log(existingLocalData);
+    // we have 3 variable: user, message and time in an array called message_data
+    console.log(Object.keys(existingLocalData).length);
+    // length of the existingLocalData Object
+    objLength = Object.keys(existingLocalData).length;
+    existingLocalData[objLength] = message_data;
+    localData[current_channel] = existingLocalData;
+    localStorage.setItem(current_channel, JSON.stringify(localData[current_channel]));
+
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <div class="row">
+        <div class="col-lg-1 pt-3">
+          <figure class="chatting__fig">
+                <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg">
+          </figure>
+          <h4 class="ml-3"><b>${ message_data[0] }</b></h4>
+        </div>
+        <div class="col-lg-10 pt-2">
+          <div class="message__data">
+              <h4 class="results__name">${ message_data[1] }</h4>
+          </div>
+        </div>
+      </div>
+    `;
+    document.querySelector('.message_list').append(li);
+  });
+
+
+  socket.on('state', data => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <p>-------------------------------------------------</p>
+      <h4 style="text-align: center;">${data.announce}</h4>
+      <p>-------------------------------------------------</p>
+    `;
+    document.querySelector('.message_list').append(li);
+    console.log(li);
+
   });
 });
