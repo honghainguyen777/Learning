d0da19820cc142e70d785ea8722e616813475406
honghainguyen777 <n.h.hai2015@gmail.com>
2020-06-14 21:58:06 +0200

    Blog

---
+++ Modern React with Redux-2020/Blog-react-redux-thunk-lodash/src/actions/index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //_.uniq is a function to return only unique ids
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // another condensed code for the above lines
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .values()
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize( async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
