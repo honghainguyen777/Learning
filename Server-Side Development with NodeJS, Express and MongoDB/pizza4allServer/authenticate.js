5e6f1276e99fa333c7fb1e31dff90989d9c21342
honghainguyen777 <n.h.hai2015@gmail.com>
2020-07-02 22:39:09 +0200

    Passport Facebook

--- Server-Side Development with NodeJS, Express and MongoDB/pizza4allServer/authenticate.js
+++ Server-Side Development with NodeJS, Express and MongoDB/pizza4allServer/authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStratery = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 const e = require('express');
@@ -55,4 +56,35 @@ exports.verifyAdmin = (req, res, next) => {
         err.status = 403;
         return next(err);
     }
-}
+};
+
+// facebook passport
+exports.facbookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName});
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    else {
+                        return done(null, user);
+                    }
+                });
+            }
+        });
+    }
+
+));
--- Server-Side Development with NodeJS, Express and MongoDB/pizza4allServer/config.js
+++ Server-Side Development with NodeJS, Express and MongoDB/pizza4allServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': 'Motxx-Thexx-Gioix-Moixx',
-    'mongoUrl': 'mongodb://localhost:27017/pizza'
+    'mongoUrl': 'mongodb://localhost:27017/pizza',
+    'facebook': {
+        clientId: '637018583688465',
+        clientSecret: 'bb5758a7f1d9c43bad9996d78f9a6d25'
+    }
 }
