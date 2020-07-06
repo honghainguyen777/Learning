6d9e51df1581fcce289464f3627f47c75ecc662d
honghainguyen777 <n.h.hai2015@gmail.com>
2020-07-06 13:09:13 +0200

    firebase server

---
+++ Server-Side Development with NodeJS, Express and MongoDB/pizza4all_Full/pizz4all-Firebase/src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyD1l8yPixFOyXvfkwVg1AxqkZJCO-Xt32U",
+  authDomain: "pizza4allapp.firebaseapp.com",
+  databaseURL: "https://pizza4allapp.firebaseio.com",
+  projectId: "pizza4allapp",
+  storageBucket: "pizza4allapp.appspot.com",
+  messagingSenderId: "333871860242",
+  appId: "1:333871860242:web:64204afe0fc617fce4e6cb",
+  measurementId: "G-CZEY78ZL53"
+};
