16bc3f7e2b6e05c1214d937e89d76901ba4abe21
honghainguyen777 <n.h.hai2015@gmail.com>
2020-07-24 12:19:36 +0200

    React-Native: Component part 1

---
+++ Multiplatform-Mobile-App-Developement-with-React-Native/pizza4all/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
