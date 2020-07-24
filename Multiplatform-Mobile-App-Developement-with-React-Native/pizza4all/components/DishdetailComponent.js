8c234d5f32c7bac1ee822cc26319aab6730fc8eb
honghainguyen777 <n.h.hai2015@gmail.com>
2020-07-24 16:29:16 +0200

    Navigation Part 1

--- Multiplatform-Mobile-App-Developement-with-React-Native/pizza4all/components/DishdetailComponent.js
+++ Multiplatform-Mobile-App-Developement-with-React-Native/pizza4all/components/DishdetailComponent.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
+
     const dish = props.dish;
-    if (dish != null) {
-        return (
-            <Card
+    
+        if (dish != null) {
+            return(
+                <Card
                 featuredTitle={dish.name}
-                image={require('./images/uthappizza.png')}
-                >
-                <Text style={{margin: 10}}>
-                    {dish.description}
-                </Text>
-            </Card>
+                image={require('./images/uthappizza.png')}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
+
+class Dishdetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    render() {
+        const dishId = this.props.route.params.dishId;
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
         );
     }
-    else {
-        return(<View />);
-    }
 }
-
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
-}
-
 export default Dishdetail;
--- Multiplatform-Mobile-App-Developement-with-React-Native/pizza4all/components/MainComponent.js
+++ Multiplatform-Mobile-App-Developement-with-React-Native/pizza4all/components/MainComponent.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
+import { Button, View, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        }
-    }
+const MenuNavigator = createStackNavigator();
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
-
-    render() {
-        return(
-            <View>
-                <Menu dishes={this.state.dishes}
-                    onPress={(dishId) => this.onDishSelect(dishId)}
-                />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
-        );
-    }
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
 }
 
+class Main extends Component {
+
+  render() {
+ 
+    return (
+        <NavigationContainer>
+            <MenuNavigatorScreen/>           
+        </NavigationContainer>
+    );
+  }
+}
+  
 export default Main;
