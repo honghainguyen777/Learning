1cfbc25bc58263d5785a7259f50e4765dac82647
honghainguyen777 <n.h.hai2015@gmail.com>
2020-09-21 20:36:54 +0200

    dropdown widget

--- Modern React with Redux-2020/widgets/src/App.js
+++ Modern React with Redux-2020/widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -17,18 +18,26 @@ const items = [
   }
 ];
 
-// export default () => {
-//   return (
-//     <div>
-//       <Accordion items={items} />
-//     </div>
-//   );
-// };
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0])
   return (
     <div>
-      <Search />
+      <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
     </div>
   );
 };
---
+++ Modern React with Redux-2020/widgets/src/components/Dropdown.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef()
+
+  // execute only once when a user clicks
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      // after the element is rendered for the first time (form div below)
+      // we can get the current reference of the div contains ref using ref.current
+      // if event.target is inside of the current element, return without doring anything
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    });
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
