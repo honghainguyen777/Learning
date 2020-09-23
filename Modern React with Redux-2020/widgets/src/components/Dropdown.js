401f8d1f9f70306e1a5a0eb74160855887de2dce
honghainguyen777 <n.h.hai2015@gmail.com>
2020-09-23 11:21:07 +0200

    translation widget

--- Modern React with Redux-2020/widgets/src/components/Dropdown.js
+++ Modern React with Redux-2020/widgets/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef()
 
@@ -46,7 +46,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{ label }</label>
         <div onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
         >
@@ -57,7 +57,6 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
           </div>
         </div>
       </div>
-      <p style={{ color: selected.value }}>This text is {selected.value}!</p>
     </div>
   );
 };
---
+++ Modern React with Redux-2020/widgets/src/components/Translate.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+import Convert from './Convert';
+
+
+const key = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
+
+
+const options = [
+  {
+    label: 'Afrikaan',
+    value: 'af'
+  },
+  {
+    label: 'Arabic',
+    value: 'ar'
+  },
+  {
+    label: 'Hindi',
+    value: 'hi'
+  },
+  {
+    label: 'Vietnamese',
+    value: 'vi'
+  },
+  {
+    label: 'German',
+    value: 'de'
+  }
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
+
+export default Translate;
